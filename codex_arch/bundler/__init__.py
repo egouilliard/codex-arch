187c993f72ac04b0803cacd3a3ec249d00958d7e
Edouard <egouilliard@virtual-tailor.com>
2025-04-11 13:00:56 +0100

    Task 6 Done

---
+++ codex_arch/bundler/__init__.py
@@ -0,0 +1,9 @@
+"""
+Context Bundle Assembler Module
+
+This module packages all generated artifacts into a structured context bundle for LLM consumption.
+"""
+
+from codex_arch.bundler.context_bundle_assembler import ContextBundleAssembler
+
+__all__ = ['ContextBundleAssembler'] 
